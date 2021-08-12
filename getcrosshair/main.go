8d4f4bf925172ca7195fcb07c16f5c81849c44ad
DerRomtester <dieb.stefan.96@gmail.com>
2021-08-12 23:09:08 +0200

    Get Demo und GetCrosshair außerhalb der Main function

--- getcrosshair/main.go
+++ getcrosshair/main.go
@@ -10,47 +10,55 @@ import (
 	events "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
 )
 
-func main() {
-
+// Collects All Demos from the Demopath and checks if the demo
+func GetDemo(demopath string) []string {
 	var demos []string
-	players_crosshair := make(map[string]string)
-	demopath := "C:/Demo/Pros"
-
 	err := filepath.Walk(demopath, func(path string, info os.FileInfo, err error) error {
-		demos = append(demos, path)
+		if strings.Contains(path, ".dem") {
+			demos = append(demos, path)
+		}
 		return nil
 	})
 	if err != nil {
 		panic(err)
 	}
+	return demos
+}
+
+// Returns the Crosshair from the Demos that get into the function
+func GetCrosshair(demos []string) map[string]string {
+	players_crosshair := make(map[string]string)
 	for _, demo := range demos {
+		f, err := os.Open(demo)
 
-		matched := strings.Contains(demo, ".dem")
-		if matched {
-			f, err := os.Open(demo)
-
-			if err != nil {
-				panic(err)
-			}
-			defer f.Close()
-
-			p := dem.NewParser(f)
-
-			p.RegisterEventHandler(func(start events.MatchStart) {
-				for _, pl := range p.GameState().Participants().All() {
-					players_crosshair[pl.Name] = pl.CrosshairCode()
-				}
-			})
-
-			// Parse to end
-			err = p.ParseToEnd()
-			if err != nil {
-				panic(err)
-			}
+		if err != nil {
+			panic(err)
 		}
-	}
+		defer f.Close()
 
-	for player, crosshair := range players_crosshair {
+		p := dem.NewParser(f)
+
+		p.RegisterEventHandler(func(start events.MatchStart) {
+			for _, pl := range p.GameState().Participants().All() {
+				players_crosshair[pl.Name] = pl.CrosshairCode()
+			}
+		})
+
+		// Parse to end
+		err = p.ParseToEnd()
+		if err != nil {
+			panic(err)
+		}
+
+	}
+	return players_crosshair
+}
+
+func main() {
+	demos := GetDemo("C:/Demo/Faceit")
+	crosshairs := GetCrosshair(demos)
+
+	for player, crosshair := range crosshairs {
 		if crosshair != "" {
 			fmt.Println("Player:", player, "   Crosshair:", crosshair)
 		}
