f8d3a0aad08da80ee898005cdb8e77169830b0a4
DerRomtester <dieb.stefan.96@gmail.com>
2023-01-08 21:56:10 +0100

    demo-service: we can use a goroutine for demo.GetCrosshairs() * it does not matter actually because we only loop over chan_playerdata when the channel is closed

--- cmd/demo-service/main.go
+++ cmd/demo-service/main.go
@@ -17,7 +17,7 @@ func main() {
 	*/
 	demos := demo.GetDemos("/home/stefan/csgo-demos")
 	chan_playerdata := make(chan []demo.PlayerInfo)
-	demo.GetCrosshairs(demos, chan_playerdata)
+	go demo.GetCrosshairs(demos, chan_playerdata)
 	for i := range chan_playerdata {
 		var Mg_DB = database.Mongo_ConnectDB()
 		var Pg_DB = database.Pg_ConnectDB()
