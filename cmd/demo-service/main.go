0ed624320a77ff59e9dffa7953bc94348d998c8e
DerRomtester <dieb.stefan.96@gmail.com>
2023-01-08 20:24:39 +0100

    demo-service: work with channels in main function and add postgresql support

--- cmd/demo-service/main.go
+++ cmd/demo-service/main.go
@@ -16,7 +16,13 @@ func main() {
 			demos := GetDemo(os.Args[1])
 	*/
 	demos := demo.GetDemos("/home/stefan/csgo-demos")
-	playerdata := demo.GetCrosshairs(demos)
-	var DB = database.ConnectDB()
-	database.WriteCrosshairDB(playerdata, DB)
+	chan_playerdata := make(chan []demo.PlayerInfo)
+	demo.GetCrosshairs(demos, chan_playerdata)
+	for i := range chan_playerdata {
+		var Mg_DB = database.Mongo_ConnectDB()
+		var Pg_DB = database.Pg_ConnectDB()
+		database.Mongo_WriteCrosshairDB(i, Mg_DB)
+		database.Pg_WriteCrosshairDB(i, Pg_DB)
+	}
+
 }
